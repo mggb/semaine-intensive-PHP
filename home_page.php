9cf96b7a9a55b9bc8248a6510206b297c20433d2
ninolam <33219320+ninolam@users.noreply.github.com>
2018-02-15 11:47:35 +0100

    Add files via upload

---
+++ home_page.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="reset.css">
+  </head>
+  <body>
+    <?php include("header.php"); ?>
+
+<section class="home-page-container">
+<div class="home-page">
+  <h1>Trouver la personne qu'il vous
+     faut pour réaliser vos projets
+     digitaux aux meilleurs prix.</h1>
+  <img class="img-computer"src="imgs/photo.jpg" alt="">
+</div>
+<div class="categories">
+  <a href="#">Développement Web</a>
+  <a href="#">Design web</a>
+</div>
+</section>
+
+  </body>
+</html>
